c6e730825d4ed212c68d185bb3988dbd6cb409bc
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-06-25 22:04:43 +0300

    1.6 Аргументы

--- src/main/java/ru/calculator/ArgMethod.java
+++ src/main/java/ru/calculator/ArgMethod.java
@@ -4,6 +4,7 @@ public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
+
     public static void main(String[] args) {
         String name = "Java";
         int age = 6;
@@ -12,6 +13,4 @@ public class ArgMethod {
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
     }
-
-
 }
