05045c3b68a9c4b3bce4c058d56d896214e2cd36
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-07-03 20:54:40 +0300

    1.11Расстояние_между_точками_в_системе_координат

--- src/main/java/ru/condition/Point.java
+++ src/main/java/ru/condition/Point.java
@@ -8,10 +8,10 @@ public class Point {
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) = " + result);
+        System.out.println("result (0, 2) to (0, 0) = " + result);
          result = Point.distance(1, 5, 0, 0);
-        System.out.println("result (1, 5) to (0, 0) = " + result);
+        System.out.println("result (1, 0) to (5, 0) = " + result);
         result = Point.distance(0, 1, 3, 0);
-        System.out.println("result (0, 1) to (3, 0) = " + result);
+        System.out.println("result (0, 3) to (1, 0) = " + result);
     }
 }
