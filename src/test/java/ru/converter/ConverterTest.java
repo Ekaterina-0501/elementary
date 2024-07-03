7af2b879a5710355cd36525378de54c43599a38a
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-07-03 19:09:32 +0300

    2.1 Модульные тесты

--- src/test/java/ru/converter/ConverterTest.java
+++ src/test/java/ru/converter/ConverterTest.java
@@ -3,8 +3,6 @@ package ru.converter;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ConverterTest {
 
     @Test
