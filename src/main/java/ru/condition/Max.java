e8bbaa704c7bba388a0b9119639a5160b67e8c55
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-07-11 06:14:46 +0300

    3.7 Тернарное сравнение

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -2,12 +2,11 @@ package ru.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left >= right ? left : right;
-        return result;
+        return left >= right ? left : right;
     }
 
     public static void main(String[] args) {
-        System.out.println(Max.max(2, 2));
+        System.out.println(Max.max(0, 2));
     }
 
 }
