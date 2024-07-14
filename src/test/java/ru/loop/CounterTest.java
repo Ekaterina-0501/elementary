fec7b4ef72f1e09c4f5b64b874212f0dbdac6f08
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-07-14 22:12:57 +0300

    4.4_Подсчет_суммы_чётных_чисел_в_диапазоне

---
+++ src/test/java/ru/loop/CounterTest.java
@@ -0,0 +1,25 @@
+package ru.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinusThenToTenThenThirty() {
+        int start = -10;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+}
