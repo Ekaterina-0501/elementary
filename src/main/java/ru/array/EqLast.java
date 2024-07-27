bbbf132482d894c6a86eb035d0da4baa075984c2
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-07-27 07:49:42 +0300

    5.15_Сравнить_последние_элементы_двух_массивов

--- src/main/java/ru/array/EqLast.java
+++ src/main/java/ru/array/EqLast.java
@@ -2,10 +2,7 @@ package ru.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        if (left[right.length - 1] == right[right.length - 1]) {
-            return true;
-        } else {
-            return false;
-        }
+        boolean result = left[right.length - 1] == right[right.length - 1];
+        return result;
     }
 }
