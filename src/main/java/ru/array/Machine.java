108ad099e8a0606815099c7e0f20fdacc49c7b31
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-08-03 14:47:23 +0300

    5.32_Жадный_алгоритм_Сдача_в_кофе_машине (1)

--- src/main/java/ru/array/Machine.java
+++ src/main/java/ru/array/Machine.java
@@ -6,15 +6,12 @@ public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
-        int i = 0;
         int size = 0;
         money -= price;
-        while (money > 0) {
-            if (money - coins[i] < 0) {
-                i++;
-            } else {
-                rsl[size] = coins[i];
-                money -= coins[i];
+        for (int coin: coins) {
+            while (money >= coin) {
+                rsl[size] = coin;
+                money -= coin;
                 size++;
             }
         }
