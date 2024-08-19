0bd55d7e537278d7dc117bf1da7523e431dbc8e5
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-08-19 19:15:37 +0300

    7.13_Рефакторинг_Расстояние_между_точками

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -7,35 +7,28 @@ class PointTest {
 
     @Test
     void when00to20then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         int expected = 2;
-        double rsl = Point.distance(x1, x2, y1, y2);
+        double rsl = a.distance(b);;
         assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when10to90then8() {
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 9;
-        int y2 = 0;
+        Point a = new Point(1, 0);
+        Point b = new Point(9, 0);
         int expected = 8;
-        double rsl = Point.distance(x1, x2, y1, y2);
+        double rsl = a.distance(b);
         assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus10to90then10() {
-        int x1 = -1;
-        int y1 = 0;
-        int x2 = 9;
-        int y2 = 0;
+        Point a = new Point(-1, 0);
+        Point b = new Point(9, 0);
         int expected = 10;
-        double rsl = Point.distance(x1, x2, y1, y2);
+        double rsl = a.distance(b);
         assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
     }
-
 }
