5c1a6e13a9793e0510a863563ba26215bd8a6a01
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-08-23 21:14:52 +0300

    7.15_Перегрузить_метод_max_для_трех_чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -5,12 +5,12 @@ public class Max {
         return left >= right ? left : right;
     }
 
-    public static int max(int left, int right, int middle) {
-        return max(left, max(right, middle));
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
     }
 
-    public static int max(int left, int right, int middle1, int middle2) {
-        return max(max(left, right, middle1), middle2);
+    public static int max(int first, int second, int third, int fourth) {
+        return max(max(first, second, third), fourth);
     }
 
     public static void main(String[] args) {
